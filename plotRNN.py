b6927d8cc3f843bcbf3226a5357af853c3a50f7f
francescorubbo <rubbo@atlint03.slac.stanford.edu>
2016-09-26 12:09:39 -0700

    update

--- plotRNN.py
+++ plotRNN.py
@@ -8,8 +8,17 @@ from utils import *
 qfile = '../data/user.rubbo.341566.root'
 gfile = '../data/user.rubbo.341565.root'
 
+minpt = 50
+maxpt = 100
+
 max_len=10
 vars = ['pt']
+
+qjets = gettracks(vars,'qjet',qfile,train=0,ptmin=minpt,ptmax=maxpt)
+gjets = gettracks(vars,'gjet',gfile,train=0,ptmin=minpt,ptmax=maxpt)
+X = concatenate( (gjets, qjets) )
+X = X[:,:max_len,:]
+y = np.concatenate( ( np.zeros(len(gjets)), np.ones(len(qjets)) ) )
 
 model = Sequential()
 model.add(LSTM(output_dim=128, activation='sigmoid', inner_activation='hard_sigmoid',
@@ -22,25 +31,27 @@ model.compile(loss='binary_crossentropy',
               optimizer='rmsprop',
               metrics=['accuracy'])
 
-model.load_weights('output/model_ntrk10_nepoch10.h5')
-
-
-
-qjets = gettracks(vars,'qjet',qfile,train=0)
-gjets = gettracks(vars,'gjet',qfile,train=0)
-X = concatenate( (gjets, qjets) )
-X = X[:,:max_len,:]
-y = np.concatenate( ( np.zeros(len(gjets)), np.ones(len(qjets)) ) )
-
-
-predicted = model.predict_proba(X,batch_size=16)
-
-from sklearn.metrics import roc_curve
-
-fpr,tpr,thres = roc_curve(y,predicted)
 
 from pylab import *
 
-plot(fpr,tpr,label='RNN')
+maxtracks = [10,20,50]
+for mt in maxtracks:
+    nepochs = [10,20,50]
+    scores = []
+    for ne in nepochs:
+        model.load_weights('output/model_ntrk%(mt)d_nepoch%(ne)d.h5'%{'mt':mt,'ne':ne})
 
+        score = model.evaluate(X, y, batch_size=16)
+        print 'score',score
+        scores.append(score[1])
+
+    plot(nepochs,scores)    
 show()
+#predicted = model.predict_proba(X,batch_size=16)
+#
+#from sklearn.metrics import roc_curve
+#fpr,tpr,thres = roc_curve(y,predicted)
+#
+#plot(fpr,tpr,label='RNN')
+#
+#show()
