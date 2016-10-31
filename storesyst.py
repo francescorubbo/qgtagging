55f0fe968bb83ee0e8bbe28d00213fd928045491
francescorubbo <rubbo.francesco@gmail.com>
2016-10-31 09:51:15 -0700

    script to generate input histogram for ncharge syst

--- storesyst.py
+++ storesyst.py
@@ -2,81 +2,88 @@ from pylab import *
 from utils import *
 from syst_dict import getsyst,applysyst
 import ROOT as r
+from array import array
 
 SetupATLAS()
 
-minpts = [200]
-maxpts = [300]
-#minpts = [50,100,200]
-#maxpts = [100,200,300]
+#minpts = [50,100]
+#maxpts = [100,200]
+minpts = [50,100,200,300,400,500,600,800,1000,1200]
+maxpts = [100,200,300,400,500,600,800,1000,1200,1500]
 filename = '../data/dijet.root'
 
-var = 'ntrk500'
-systname = 'pdf'
+var = 'ntrk'
+systname = 'exp'
 
-bins = np.linspace(-0.5,30.5,32)
-bincenters = (bins+0.5)[:-1]
+ptbins = [50,100,200,300,400,500,600,800,1000,1200,1500]
+nbins = 81
+ntrkbins = np.linspace(-0.5,nbins-0.5,nbins+1).tolist()
 
-#ptbins = [50,100,200,300,400,500,600,800,1000,1200,1500]
-qntrkbins = {
-    50: 15,
-    100: 20,
-    200: 25,
-    300: 30,
-    400: 30,
-    500: 35,
-    600: 40,
-    800: 45,
-    1000: 50,
-    1200: 55,
-    1500: 60
-    }
+fout = r.TFile('qgsyst_'+systname+'.root','RECREATE')
+h2dquark_up   = r.TH2D('h2dquark_up'  ,'h2dquark_up'  ,len(ptbins)-1,array('d',ptbins),nbins,array('d',ntrkbins))
+h2dquark_down = r.TH2D('h2dquark_down','h2dquark_down',len(ptbins)-1,array('d',ptbins),nbins,array('d',ntrkbins))
+h2dgluon_up   = r.TH2D('h2dgluon_up'  ,'h2dgluon_up'  ,len(ptbins)-1,array('d',ptbins),nbins,array('d',ntrkbins))
+h2dgluon_down = r.TH2D('h2dgluon_down','h2dgluon_down',len(ptbins)-1,array('d',ptbins),nbins,array('d',ntrkbins))
 
-gntrkbins = {
-    50: 18,
-    100: 25,
-    200: 30,
-    300: 35,
-    400: 40,
-    500: 45,
-    600: 45,
-    800: 50,
-    1000: 60,
-    1200: 65,
-    1500: 70
-    }
-
-fout = r.TFile('test.root','RECREATE')
-
-for minpt,maxpt in zip(minpts,maxpts):
-
-    hquark      = r.TH1I('hquark'     ,'hquark'     ,qntrkbins[minpt],0,qntrkbins[minpt])
-    hquark_up   = r.TH1I('hquark_up'  ,'hquark_up'  ,qntrkbins[minpt],0,qntrkbins[minpt])
-    hquark_down = r.TH1I('hquark_down','hquark_down',qntrkbins[minpt],0,qntrkbins[minpt])
-    hgluon      = r.TH1I('hgluon'     ,'hgluon'     ,gntrkbins[minpt],0,gntrkbins[minpt])
-    hgluon_up   = r.TH1I('hgluon_up'  ,'hgluon_up'  ,gntrkbins[minpt],0,gntrkbins[minpt])
-    hgluon_down = r.TH1I('hgluon_down','hgluon_down',gntrkbins[minpt],0,gntrkbins[minpt])
+for iptbin,(minpt,maxpt) in enumerate(zip(minpts,maxpts)):
+    prefix = systname+'_%d_'%minpt
+    hquark      = r.TH1D(prefix+'hquark'     ,prefix+'hquark'     ,nbins,array('d',ntrkbins))
+    hquark_up   = r.TH1D(prefix+'hquark_up'  ,prefix+'hquark_up'  ,nbins,array('d',ntrkbins))
+    hquark_down = r.TH1D(prefix+'hquark_down',prefix+'hquark_down',nbins,array('d',ntrkbins))
+    hgluon      = r.TH1D(prefix+'hgluon'     ,prefix+'hgluon'     ,nbins,array('d',ntrkbins))
+    hgluon_up   = r.TH1D(prefix+'hgluon_up'  ,prefix+'hgluon_up'  ,nbins,array('d',ntrkbins))
+    hgluon_down = r.TH1D(prefix+'hgluon_down',prefix+'hgluon_down',nbins,array('d',ntrkbins))
 
     #quarks
     syst = getsyst('quark',maxpt,systname)
     systup = syst[0]
     systdown = syst[1]
-    qjets = getvar(var,'qjet',filename,reco=True,ptmin=minpt,ptmax=maxpt)
+    qjets = getvar(var,'qjet',filename,reco=False,ptmin=minpt,ptmax=maxpt)
     qjets_up = applysyst(1.,systup,qjets)
     qjets_down = applysyst(-1.,systdown,qjets)
     for q in qjets: hquark.Fill(q)
     for q in qjets_up: hquark_up.Fill(q)
     for q in qjets_down: hquark_down.Fill(q)
+    #hquark.Write()
+    #hquark_up.Write()
+    #hquark_down.Write()
+    for ibin in range(1,nbins+2):
+        val = hquark.GetBinContent(ibin)
+        ratioup = hquark_up.GetBinContent(ibin)/val if val>5 else 1.
+        ratiodown = hquark_down.GetBinContent(ibin)/val if val>5 else 1.
+        h2dquark_up.SetBinContent(iptbin+1,ibin,ratioup)
+        h2dquark_down.SetBinContent(iptbin+1,ibin,ratiodown)
 
     #gluons
     syst = getsyst('gluon',maxpt,systname)
     systup = syst[0]
     systdown = syst[1]
-    gjets = getvar(var,'gjet',filename,reco=True,ptmin=minpt,ptmax=maxpt)
-    gjets_systup = applysyst(1.,systup,gjets)
-    gjets_systdown = applysyst(-1.,systdown,gjets)
-    for q in qjets: hquark.Fill(q)
-    for q in qjets_up: hquark_up.Fill(q)
-    for q in qjets_down: hquark_down.Fill(q)
+    gjets = getvar(var,'gjet',filename,reco=False,ptmin=minpt,ptmax=maxpt)
+    gjets_up = applysyst(1.,systup,gjets)
+    gjets_down = applysyst(-1.,systdown,gjets)
+    for g in gjets: hgluon.Fill(g)
+    for g in gjets_up: hgluon_up.Fill(g)
+    for g in gjets_down: hgluon_down.Fill(g)
+    #hgluon.Write()
+    #hgluon_up.Write()
+    #hgluon_down.Write()
+    for ibin in range(1,nbins+2):
+        val = hgluon.GetBinContent(ibin)
+        ratioup = hgluon_up.GetBinContent(ibin)/val if val>5 else 1.
+        ratiodown = hgluon_down.GetBinContent(ibin)/val if val>5 else 1.
+        h2dgluon_up.SetBinContent(iptbin+1,ibin,ratioup)
+        h2dgluon_down.SetBinContent(iptbin+1,ibin,ratiodown)
+    
+#set pT overflow to last bin value
+for ibin in range(1,nbins+2):
+    h2dquark_up.SetBinContent(h2dquark_up.GetNbinsX()+1,ibin,h2dquark_up.GetBinContent(h2dquark_up.GetNbinsX(),ibin))
+    h2dquark_down.SetBinContent(h2dquark_down.GetNbinsX()+1,ibin,h2dquark_down.GetBinContent(h2dquark_down.GetNbinsX(),ibin))
+    h2dgluon_up.SetBinContent(h2dgluon_up.GetNbinsX()+1,ibin,h2dgluon_up.GetBinContent(h2dgluon_up.GetNbinsX(),ibin))
+    h2dgluon_down.SetBinContent(h2dgluon_down.GetNbinsX()+1,ibin,h2dgluon_down.GetBinContent(h2dgluon_down.GetNbinsX(),ibin))
 
-fout.Write()
+h2dquark_up.Write()
+h2dquark_down.Write()
+h2dgluon_up.Write()
+h2dgluon_down.Write()
+
+#fout.Write()
